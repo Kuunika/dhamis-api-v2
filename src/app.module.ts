fb247c12d23e2d59204848f58a63c51367cd9adf
Brett Onions <onionsllb@outlook.com>
2021-01-22 11:29:37 +0200

    Setting Up TypeORM and Creating HCC interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HivCareClinicModule } from './hiv-care-clinic/hiv-care-clinic.module';
 import { DhaArtSectionService } from './dhamis/dha-art-section/dha-art-section.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+import { DEFAULT_DB_SERVER_PORT } from './common/constants/configuration.constants';
 
+config();
 @Module({
-  imports: [HivCareClinicModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT ?? DEFAULT_DB_SERVER_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: false,
+    }),
+    HivCareClinicModule,
+  ],
   controllers: [AppController],
   providers: [AppService, DhaArtSectionService],
 })
---
+++ src/common/interfaces/hcc-registration.d.ts
@@ -0,0 +1,7 @@
+export interface HccRegistration {
+  quarter: number;
+  hfacility: number;
+  concept: string;
+  data_value: number;
+  product_code: string;
+}
