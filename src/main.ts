b71497ae9451f5f9fb5d2e0c8f9ee0f9b3662e48
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2021-06-02 09:00:54 +0200

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { env } from './common/helper';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('dhamis/v2/api');
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true
+  });
   const server = await app.listen(env('PORT') ?? 3000);
   server.setTimeout(25_000);
 }
